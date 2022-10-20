c745798726b56a52aef009489f2d0c69a5d25fca
jaelps <jaelsonpsilva18@gmail.com>
2022-10-20 17:43:28 -0300

    inicial

---
+++ ex2.py
@@ -0,0 +1,15 @@
+#Escreva um programa que leia um vetor V (contendo 30 inteiros) e um 
+# valor inteiro K, calcule e exiba a quantidade de ocorrências de K 
+# dentro de V.
+
+n = 5 
+
+v = [None]*n
+
+print('Digite os elementos do vetor A:  ')
+for i in range(n):
+    v[i] = int(input())
+
+
+    
+
